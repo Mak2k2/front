a294b29d6ff74c21e2aed0ec5cc9a4501cccd1ec
Mak2k2 <mak2k2@ya.ru>
2020-10-23 16:18:40 +0300

    Syn

---
+++ src/scripts/slider_middle.js
@@ -0,0 +1,7 @@
+$(document).on('DOMContentLoaded', () => {
+	$(".slider_middle").slick({
+    infinity: true,
+    slidesToShow: 3,
+	  slidesToScroll: 3,
+  });
+});
---
+++ src/scripts/test.js
@@ -0,0 +1,4 @@
+console.log("start JS");
+if (window.jQuery) {
+  console.log("test jquery");
+}

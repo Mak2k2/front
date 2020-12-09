2a87ab6da1a37ebda60fa5b9d33725403ad30ce8
Mak2k2 <31064571+Mak2k2@users.noreply.github.com>
2020-12-09 22:09:13 +0300

    Old

---
+++ back/src/scripts/popup.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+	$('.show-popup').magnificPopup({
+		type: 'inline',
+		removalDelay: 500,
+		callbacks: {
+			beforeOpen: function () {
+				this.st.mainClass = this.st.el.attr('data-effect');
+			}
+		},
+		midClick: true
+	});
+});
